a424a7fce6e0dffb99cb40d0573734e0407f4ce6
Vitor Martins da Costa Fernandes <vitormartins@Vitors-MBP.Home>
2024-07-18 16:23:31 +0100

    feat: add env variables management

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -2,6 +2,7 @@ import { fastifyCors } from '@fastify/cors'
 import { fastifyJwt } from '@fastify/jwt'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import { env } from '@saas/env'
 import fastify from 'fastify'
 import {
   jsonSchemaTransform,
@@ -49,7 +50,7 @@ app.register(fastifySwaggerUi, {
 })
 
 app.register(fastifyJwt, {
-  secret: 'my-jwt-secret',
+  secret: env.JWT_SECRET,
 })
 
 app.register(fastifyCors)
@@ -60,6 +61,6 @@ app.register(getProfile)
 app.register(requestPasswordRecover)
 app.register(resetPassword)
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('Server is running on port 3333')
 })
